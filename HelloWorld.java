83742dd58031b3ef968fd3bebd51b9b016b3ff19
Sandhyakoshta99 <115773353+Sandhyakoshta99@users.noreply.github.com>
2022-10-14 19:44:44 +0530

    12 oct

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class  HelloWorld //declaring a class
+{
+public static void main(String agrs[])  //main method
+{
+System.out.println("Hello World");  //print the value
+}   //end main
+}    //end of class HelloWorld
---
+++ Student.java
@@ -0,0 +1,9 @@
+class Student //declaring class
+{
+public static void main(String args[])  //main method
+    {
+       System.out.println("Name" +args[0]);   //printin the value 
+       System.out.println("Age" +args[1]);
+       System.out.println("Course" +args[2]);
+    }//end main
+}    //end of class Student
