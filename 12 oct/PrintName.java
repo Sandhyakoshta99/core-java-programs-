44974a8311749b36df67540b1c486403a8639aed
Sandhyakoshta99 <115773353+Sandhyakoshta99@users.noreply.github.com>
2022-10-15 18:34:06 +0530

    Add files via upload

---
+++ 12 oct/PrintName.java
@@ -0,0 +1,7 @@
+class PrintName //declaring a class
+{
+public static final void main(String args[])  //main method
+     {
+      System.out.println( "wellcome "+args[0] );   //print the value
+     }    //end main
+}   //end of class PrintName
