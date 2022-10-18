633f216db4f6381439074e2a732151ef27e7e475
Sandhyakoshta99 <115773353+Sandhyakoshta99@users.noreply.github.com>
2022-10-19 00:14:28 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class  HelloWorld //declaring a class
-{
-public static void main(String agrs[])  //main method
-{
-System.out.println("Hello World");  //print the value
-}   //end main
-}    //end of class HelloWorld
