8440d844da83465b72487ad14be05ff1648ce183
Sandhyakoshta99 <115773353+Sandhyakoshta99@users.noreply.github.com>
2022-10-19 00:15:21 +0530

    Delete Student.java

--- Student.java
+++
@@ -1,8 +0,0 @@
-class Student //declaring class
-{
-public static void main(String args[])  //main method 
-{
-System.out.println("Name:" +args[0]+" "+args[1]); //printing the values
-System.out.println("class" +args[2]); 
-}//end main
-} //end of class Student
