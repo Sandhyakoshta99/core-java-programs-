8d56188218f04e7ada31bf25d0b22945ac1787e6
Sandhyakoshta99 <115773353+Sandhyakoshta99@users.noreply.github.com>
2022-10-19 00:14:55 +0530

    Delete PrintName.java

--- PrintName.java
+++
@@ -1,7 +0,0 @@
-class PrintName //declaring a class
-{
-public static final void main(String args[])  //main method
-     {
-      System.out.println( "wellcome "+args[0] );   //print the value
-     }    //end main
-}   //end of class PrintName
