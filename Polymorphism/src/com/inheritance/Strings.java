5b3b8a1f09105fcca7f9cfc3cc1502f230b18c44
sandhya koshta <sandhyakoshta44@gmail.com>
2022-11-09 11:26:06 +0530

    ok

---
+++ Polymorphism/src/com/inheritance/Strings.java
@@ -0,0 +1,21 @@
+package com.inheritance;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int i=7;
+     i++;
+     i--;
+     System.out.println(i);
+     int j=7;
+         j--;
+     System.out.println(j);
+     
+     String name="Anudip ";
+     name=name.concat("Foundation");
+     System.out.println(name);
+     
+	}
+
+}
